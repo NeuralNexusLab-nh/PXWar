3b687a60a6394fda2a00556e79cc86b64e1c6883
NeuralNexusLab <neuralnexuslab@hotmail.com>
2026-02-10 18:22:44 +0800

    Update server.js

--- server.js
+++ server.js
@@ -3,11 +3,22 @@ const path = require("path");
 const app = express();
 
 app.all("/download", (req, res) => {
-  res.download(path.join(__dirname, "pages", "index.html"));
+  res.download(path.join(__dirname, "pages", "pxwar.html"));
+});
+
+app.all("/domains", (req, res) => {
+  res.set("Content-Type", "text/plain");
+  res.send(`
+  https://pxwar.ndjp.net
+  https://pxwar.is-an.ai
+  https://pxwar.zone.id
+  https://pxwar.zeabur.app
+  https://pxwar.ddnsfree.com
+  `);
 });
 
 app.all("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "pages", "index.html"));
+  res.sendFile(path.join(__dirname, "pages", "pxwar.html"));
 });
 
 const PORT = process.env.PORT || 8080;
